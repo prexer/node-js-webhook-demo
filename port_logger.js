d32aeb0aac82201621fa12e39f4355041684de30
Peter Rexer <prexer@gmail.com>
2012-02-15 08:41:58 -0800

    again

--- port_logger.js
+++ port_logger.js
@@ -1,12 +1,32 @@
-var express = require('express')
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-	response.send('Starting Up Echo Server');
-});
-
+var http = require('http');
+var url = require('url');
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-	console.log("Listening on "+ port);
+
+var logs=[];
+var other=[];
+
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	
+	if (req.url == "/log"){
+		logs.push(res)
+		console.log('added to the log array');
+
+	} else {
+		var query = url.parse(req.url).query
+		if (query != undefined){
+			console.log('got a query for %s', query);
+	
+			for (var i=0;i<logs.length;i++){
+				logs[i].write(query);
+				logs[i].write('<br/>\n');
+			}
+		}
+		res.end('ok');
+	}
+	
 });
+
+server.listen(port);
+
+console.log('server running at %s', port);
