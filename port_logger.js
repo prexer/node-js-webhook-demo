9a2c99556975e7158d78f727fa8d5264b7ab623b
Peter Rexer <prexer@gmail.com>
2012-03-14 19:43:18 -0700

    lots of cleanup to make the tmp file work

--- port_logger.js
+++ port_logger.js
@@ -7,19 +7,20 @@ var port = process.env.PORT || 3000;
 var server = http.createServer(function(req, res){
 	
 	if (req.url == "/log"){
-		console.log('another log client');
+		console.log('log client connected');
 	    res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(logview);
 	} else if (req.url == "/tmp/logfile.txt"){
 	    console.log('ajax request');
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.end(fs.readFileSync('./tmp/logfile.txt'));
+	    res.writeHead(200, {'Content-Type': 'text/plain'});
+	    res.end(fs.readFileSync('/tmp/logfile.txt'));
 	} else {
+		console.log('got a request for %s', req.url);
 		var query = url.parse(req.url).query;
 		if (query != undefined){
 			console.log('got a query for %s', query);
-			var log = fs.createWriteStream('log.txt', {'flags': 'a'});
-			log.write(query);	
+			var log = fs.createWriteStream('/tmp/logfile.txt', {'flags': 'a'});
+			log.write(query + "\n");	
 		}
 	    res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.end(query + ':: Logged');
